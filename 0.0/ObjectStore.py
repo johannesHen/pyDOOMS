3652c94b76d7dad90e1d12e8603852d01ec80a53
Oskar <oskare90@gmail.com>
2013-10-30 14:22:15 +0100

    Documentation

--- 0.0/ObjectStore.py
+++ 0.0/ObjectStore.py
@@ -1,6 +1,14 @@
 
 class ObjectStore:
+    """
+    Class containing a dictionary of all shared objects and methods to manipulate it.
+    Objects are indexed by their IDs.
+    """
+
     objects = dict()
 
     def addObject(self, object):
+        """
+        Adds the object to the object store dictionary
+        """
         self.objects[object.ID] = object
